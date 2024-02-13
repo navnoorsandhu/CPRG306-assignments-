68fb135d91c27db4e256246f22454b0c582f346a
navnoorsandhu <navnoor.singh@edu.sait.ca>
2024-02-12 21:28:59 -0700

    week-4

---
+++ app/week-4/page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NewItem from './new-item';
+
+const Page = () => {
+  return (
+    <div>
+      <h1>Week-4</h1>
+      <NewItem />
+    </div>
+  );
+};
+
+export default Page;
